e272e7f015d9b8bc323af3a8b77b6cef417801c0
Engin Yenice <enginyenice2626@gmail.com>
2021-05-17 00:21:38 +0300

    CACHE CLEAR

--- entities/statistics.py
+++
@@ -1,9 +0,0 @@
-class Statistics:
-    X = []
-    Gains = []
-    Steps = []
-
-    def add(gain,step):
-        Statistics.Gains.append(gain)
-        Statistics.Steps.append(step)
-        Statistics.X.append(len(Statistics.X) + 1)
